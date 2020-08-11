f7a12c7d8515ef4eb22bb1eefc15bdafab6721db
Mawuvideka <61460954+Mawuvideka@users.noreply.github.com>
2020-08-11 15:29:06 +0000

    Updates

---
+++ precache-manifest.b81a2c334af44adb536c58bcdabd787f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cc2d957cb5f3f4c5a605391ff3180b",
+    "url": "/my-reactapp/index.html"
+  },
+  {
+    "revision": "135613fabec7d0ef303d",
+    "url": "/my-reactapp/static/css/main.b1d6d1c3.chunk.css"
+  },
+  {
+    "revision": "59e616448e73f0e26f6a",
+    "url": "/my-reactapp/static/js/2.078294fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-reactapp/static/js/2.078294fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "135613fabec7d0ef303d",
+    "url": "/my-reactapp/static/js/main.898709a9.chunk.js"
+  },
+  {
+    "revision": "2b95c5c152e6efac5ab4",
+    "url": "/my-reactapp/static/js/runtime-main.89b30ccf.js"
+  },
+  {
+    "revision": "157acc88fdbb260d1108f231a392d61b",
+    "url": "/my-reactapp/static/media/CROPPED-IMG_3366.157acc88.JPG"
+  },
+  {
+    "revision": "d015089d7b796160db0397dd64cad02d",
+    "url": "/my-reactapp/static/media/DSC_0099.d015089d.jpg"
+  },
+  {
+    "revision": "b355937cd19c440b5cfbb2b856670b34",
+    "url": "/my-reactapp/static/media/image.b355937c.jpg"
+  }
+]);
